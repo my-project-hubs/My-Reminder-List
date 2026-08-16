6da6b3f6b8b4142a86a68a7d0c80b4ebca8f4f4f
my-project-hubs <indaxay8@gmail.com>
2026-08-16 22:06:27 +0530

    Add files via upload

--- api/notify.js
+++ api/notify.js
@@ -175,5 +175,4 @@ export default async function handler(req, res) {
   } catch (err) {
     return res.status(500).json({ ok: false, error: String(err) });
   }
-                                                 }
-  
+}
